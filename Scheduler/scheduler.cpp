10afd1ae9c3c5629d792f4be32064daf591ce664
IVDavidIV <144590763+IVDavidIV@users.noreply.github.com>
2025-04-14 23:20:41 +0400

    Update scheduler.cpp

--- Scheduler/scheduler.cpp
+++ Scheduler/scheduler.cpp
@@ -2,41 +2,33 @@
 
 JobScheduler::JobScheduler() {}
 
-JobScheduler::~JobScheduler() {
-    _jobList.clear();
-}
+JobScheduler::~JobScheduler() {}
 
 void JobScheduler::submitProcess(const Process& p) {
-    _jobList.push_back(p);
-}
-
-void JobScheduler::submitBatch(const std::vector<Process>& batch) {
-    for (const auto& p : batch) {
-        _jobList.push_back(p);
-    }
-}
-
-std::vector<Process> JobScheduler::getScheduledJobs() const {
-    return _jobList;
+    _jobQueue.push(p);
 }
 
 void JobScheduler::runSJF() {
-    std::sort(_jobList.begin(), _jobList.end(), [](const Process& a, const Process& b) {
-        return a.burstTime < b.burstTime;
-    });
-
     int currentTime = 0;
 
-    for (auto& job : _jobList) {
-        job.waitingTime = currentTime;
-        currentTime += job.burstTime;
-        job.completionTime = currentTime;
-        job.turnaroundTime = job.completionTime; 
+    std::vector<Process> finishedJobs;
+
+    while (!_jobQueue.empty()) {
+        Process current = _jobQueue.top();
+        _jobQueue.pop();
+
+        current.waitingTime = currentTime;
+        currentTime += current.burstTime;
+        current.completionTime = currentTime;
+        current.turnaroundTime = current.completionTime;
+
+        finishedJobs.push_back(current);
     }
 
-    std::cout << "\n=== SJF Scheduling Results ===\n";
+    std::cout << "\n=== SJF Scheduling Results (Using Priority Queue) ===\n";
     std::cout << "PID\tBurst\tWaiting\tTurnaround\tCompletion\n";
-    for (const auto& job : _jobList) {
+
+    for (const auto& job : finishedJobs) {
         std::cout << job.pid << "\t"
                   << job.burstTime << "\t"
                   << job.waitingTime << "\t"
